9c0cf23e3309f16c29362772724b188bace1e848
Luke <lukesnapacc@gmail.com>
2024-10-04 16:29:05 +0200

    TRAC-29 Refactor logger.js to add logging to file

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,8 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
 const colors = {
     INFO: '\x1b[32m', // Grün
     WARN: '\x1b[33m', // Gelb
     ERROR: '\x1b[31m', // Rot
     RESET: '\x1b[0m', // Zurücksetzen
+};
+
+const logDirectory = path.join(__dirname, 'logs');
+if (!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory);
+}
+
+const getLogFileName = () => {
+    const now = new Date();
+    return path.join(logDirectory, `${now.toISOString().split('T')[0]}.log`);
+};
+
+const logToFile = (message) => {
+    const logFileName = getLogFileName();
+    fs.appendFileSync(logFileName, message + '\n', 'utf8');
 };
 
 const log = (type, message, ...args) => {
@@ -17,15 +36,25 @@ const log = (type, message, ...args) => {
         hour12: false,
     })}.${now.getMilliseconds().toString().padStart(3, '0')}`;
     const color = colors[type] || colors.RESET;
+    const logMessage = `[${timestamp}] [${type}] ${message}`;
     console.log(
         `[${timestamp}] [${color}${type}${colors.RESET}] ${message}`,
         ...args
     );
+    logToFile(logMessage);
 };
 
 const info = (message, ...args) => log('INFO', message, ...args);
 const warn = (message, ...args) => log('WARN', message, ...args);
-const error = (message, ...args) => log('ERROR', message, ...args);
+const error = (message, err, ...args) => {
+    const errorId = uuidv4();
+    const errorMessage = `Error ID: ${errorId} - ${message}`;
+    log('ERROR', errorMessage, ...args);
+    if (err && err.stack) {
+        logToFile(err.stack);
+    }
+    return errorId;
+};
 
 module.exports = {
     info,
