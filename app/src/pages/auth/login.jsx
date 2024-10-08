448990cb1ebf5efb2637cb63608d86e8aa71b11d
Luke <lukesnapacc@gmail.com>
2024-10-08 16:46:57 +0200

    TRAC-27 Refactor login page to update styling and improve accessibility

--- app/src/pages/auth/login.jsx
+++ app/src/pages/auth/login.jsx
@@ -1,6 +1,6 @@
-import { useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { UserContext } from '../../context/userContext';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 import { useToast } from '../../context/toastContext';
 
@@ -31,67 +31,100 @@ const Login = () => {
                 addToast('Login erfolgreich!', 'success');
             }
         } catch (error) {
-            addToast('Login failed. Please try again.', 'error');
+            addToast(
+                'Login fehlgeschlagen. Bitte versuchen Sie es erneut.',
+                'error'
+            );
         }
     };
+
     return (
-        <div className='flex justify-center items-center h-screen bg-gray-100'>
-            <div className='w-full max-w-md p-8 space-y-6 bg-gradient-to-r from-blue-300 to-red-300 bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-lg shadow-md'>
-                <h2 className='text-center text-3xl font-bold text-white'>
-                    TrackMyGoal
-                </h2>
-                <form className='mt-8 space-y-6' onSubmit={loginUser}>
-                    <div className='rounded-md shadow-sm -space-y-px'>
-                        <div>
-                            <label htmlFor='email-address' className='sr-only'>
-                                Email address
-                            </label>
-                            <input
-                                id='email-address'
-                                name='email'
-                                type='email'
-                                required
-                                className='appearance-none rounded-none relative block w-full px-3 py-2 border border-transparent bg-white bg-opacity-20 text-white placeholder-white rounded-t-md focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm'
-                                placeholder='Email address'
-                                value={data.email}
-                                onChange={(event) => {
-                                    setData({
-                                        ...data,
-                                        email: event.target.value,
-                                    });
-                                }}
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor='password' className='sr-only'>
-                                Password
-                            </label>
-                            <input
-                                id='password'
-                                name='password'
-                                type='password'
-                                required
-                                className='appearance-none rounded-none relative block w-full px-3 py-2 border border-transparent bg-white bg-opacity-20 text-white placeholder-white rounded-b-md focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm'
-                                placeholder='Password'
-                                value={data.password}
-                                onChange={(event) => {
-                                    setData({
-                                        ...data,
-                                        password: event.target.value,
-                                    });
-                                }}
-                            />
+        <div className='min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-300 via-purple-300 to-red-300'>
+            <div className='absolute inset-0 bg-white opacity-20 backdrop-filter backdrop-blur-sm'></div>
+            <div className='relative w-full max-w-md'>
+                <div className='absolute inset-0 bg-white opacity-75 rounded-xl shadow-2xl transform -rotate-6'></div>
+                <div className='relative bg-white bg-opacity-90 rounded-xl shadow-xl overflow-hidden'>
+                    <div className='px-10 pt-10 pb-8'>
+                        <h2 className='text-center text-4xl font-extrabold text-blue-600 mb-8'>
+                            TrackMyGoal
+                        </h2>
+                        <form onSubmit={loginUser} className='space-y-6'>
+                            <div>
+                                <label
+                                    htmlFor='email'
+                                    className='block text-sm font-medium text-gray-700'
+                                >
+                                    E-Mail-Adresse
+                                </label>
+                                <input
+                                    id='email'
+                                    name='email'
+                                    type='email'
+                                    required
+                                    className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500'
+                                    value={data.email}
+                                    onChange={(e) =>
+                                        setData({
+                                            ...data,
+                                            email: e.target.value,
+                                        })
+                                    }
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor='password'
+                                    className='block text-sm font-medium text-gray-700'
+                                >
+                                    Passwort
+                                </label>
+                                <input
+                                    id='password'
+                                    name='password'
+                                    type='password'
+                                    required
+                                    className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500'
+                                    value={data.password}
+                                    onChange={(e) =>
+                                        setData({
+                                            ...data,
+                                            password: e.target.value,
+                                        })
+                                    }
+                                />
+                            </div>
+                            <div className='flex items-center justify-between'>
+                                <div className='text-sm'>
+                                    <Link
+                                        to='/forgot-password'
+                                        className='font-medium text-blue-600 hover:text-blue-500'
+                                    >
+                                        Passwort vergessen?
+                                    </Link>
+                                </div>
+                            </div>
+                            <div>
+                                <button
+                                    type='submit'
+                                    className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                                >
+                                    Einloggen
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className='px-10 py-4 bg-gray-50 border-t border-gray-200 flex justify-center'>
+                        <div className='text-sm'>
+                            Noch kein Konto?{' '}
+                            <Link
+                                to='/register'
+                                className='font-medium text-blue-600 hover:text-blue-500'
+                            >
+                                Jetzt registrieren
+                            </Link>
                         </div>
                     </div>
-                    <div>
-                        <button
-                            type='submit'
-                            className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white from-red-300 to-blue-300 bg-opacity-80 backdrop-filter backdrop-blur-lg shadow-lg'
-                        >
-                            Einloggen
-                        </button>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
     );
