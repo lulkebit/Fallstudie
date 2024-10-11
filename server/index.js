0f351d94c33e8f95f4912da2cc99fd427cbedd6a
Luke <lukesnapacc@gmail.com>
2024-10-11 13:33:52 +0200

    TRAC-69 Remove comments

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
-// Importiert express, eine Bibliothek zum Erstellen von Webservern und APIs
 const express = require('express');
 
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
-// Erstellt eine Express-Anwendung
 const app = express();
 
 const logger = require('./utils/logger');
@@ -21,19 +19,14 @@ mongoose
         logger.error(texts.ERRORS.ERROR('Datenbankverbindung aufbauen'), err);
     });
 
-// Middleware zum Parsen von JSON-Daten im Anfragekörper
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-// Verwendet die Authentifizierungsrouten aus der Datei './routes/authRoutes'
-// Alle Anfragen, die an die Wurzel ('/') gesendet werden, werden an den Router weitergeleitet
 app.use('/', require('./routes/routes'));
 
-// Definiert den Port, auf dem der Server läuft
 const port = 8000;
 
-// Startet den Server und gibt eine Nachricht in der Konsole aus, dass der Server läuft
 app.listen(port, () => {
     logger.info(texts.SERVER.RUNNING(port));
 });
--- server/models/user.js
+++ server/models/user.js
@@ -1,26 +1,18 @@
-// Importiere mongoose, eine Bibliothek zur Verwaltung von MongoDB-Datenbanken
 const mongoose = require('mongoose');
 
-// Destrukturiere Schema aus mongoose, um ein neues Schema für den Benutzer zu definieren
 const { Schema } = mongoose;
 
-// Definiere das Schema für das User-Modell, das die Struktur der Benutzerdaten in der Datenbank beschreibt
 const userSchema = new Schema({
-    // Definiert das Feld 'username', das vom Typ String ist und in der Datenbank einzigartig sein muss
     username: {
         type: String,
-        unique: true, // Stellt sicher, dass der Benutzername eindeutig ist
+        unique: true,
     },
-    // Definiert das Feld 'email', das ebenfalls vom Typ String ist und einzigartig sein muss
     email: {
         type: String,
-        unique: true, // Stellt sicher, dass die E-Mail-Adresse eindeutig ist
+        unique: true,
     },
-    // Speichert das gehashte Passwort als String
     password: String,
-    // Speichert den Vornamen des Benutzers
     firstname: String,
-    // Speichert den Nachnamen des Benutzers
     lastname: String,
     goals: [
         {
@@ -40,7 +32,7 @@ const userSchema = new Schema({
         },
     ],
     avatar: {
-        type: String, // Store the URL or base64 string of the avatar
+        type: String,
     },
     friends: [
         {
@@ -50,8 +42,6 @@ const userSchema = new Schema({
     ],
 });
 
-// Erstellt ein Mongoose-Modell namens 'User' basierend auf dem definierten Schema
 const UserModel = mongoose.model('User', userSchema);
 
-// Exportiere das User-Modell, damit es in anderen Modulen verwendet werden kann
 module.exports = UserModel;
