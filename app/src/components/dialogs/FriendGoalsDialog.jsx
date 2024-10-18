e8ecce2f2384ed9b6c09d6bcbfb2ee0e5938c3af
Luke <lukesnapacc@gmail.com>
2024-10-18 17:07:49 +0200

    TRAC-53 Refactor FriendGoalsDialog component to add z-index for better layering

--- app/src/components/dialogs/FriendGoalsDialog.jsx
+++ app/src/components/dialogs/FriendGoalsDialog.jsx
@@ -21,7 +21,7 @@ const FriendGoalsDialog = ({ friendId, onClose }) => {
     }, [friendId]);
 
     return (
-        <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4'>
+        <div className='fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center justify-center p-4'>
             <div className='bg-white rounded-lg shadow-xl w-full max-w-2xl max-h-[90vh] flex flex-col'>
                 <div className='p-6 border-b border-gray-200'>
                     <h2 className='text-2xl font-bold text-gray-800'>
