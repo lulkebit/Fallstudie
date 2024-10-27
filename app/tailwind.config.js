efa2e3c840d450820010776373f150ac0c6d6fcc
Luke <lukesnapacc@gmail.com>
2024-10-27 12:49:56 +0100

    TRAC-114 Refactor font loading and optimize CSS in index.html and tailwind.config.js

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,14 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: 'class',
-    content: [
-        './src/**/*.{js,jsx}', // Dies stellt sicher, dass Tailwind alle React-Komponenten scannt
-    ],
+    content: ['./src/**/*.{js,jsx}'],
     purge: {
         enabled: true,
-        content: ['./src/**/*.jsx'],
+        content: ['./src/**/*.jsx', './src/**/*.js', './public/index.html'],
         options: {
             keyframes: true,
+            fontFace: true,
+            safelist: [/^bg-/, /^text-/, /^border-/, /^hover:/, /^dark:/],
+        },
+    },
+    future: {
+        hoverOnlyWhenSupported: true,
+    },
+    theme: {
+        extend: {
+            screens: {
+                '3xl': '1920px',
+            },
         },
     },
 };
