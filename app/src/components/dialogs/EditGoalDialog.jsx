9603d5877cdd1733bdee7e28dccd579a6d0d4640
Jean-Luc <s231843@student.dhbw-mannheim.de>
2024-10-29 18:04:12 +0100

    TRAC-122 Icons sind jetzt weiß im Darkmode

--- app/src/components/dialogs/EditGoalDialog.jsx
+++ app/src/components/dialogs/EditGoalDialog.jsx
@@ -122,7 +122,7 @@ const InputField = React.memo(
             </label>
             <div className='relative'>
                 <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
-                    <Icon className='h-5 w-5 text-gray-400 dark:text-white/40' />
+                    <Icon className='h-5 w-5 text-gray-400 dark:text-white' />
                 </div>
                 {type === 'textarea' ? (
                     <textarea
